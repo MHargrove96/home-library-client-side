1197dfce6ca03a6b28e403e27493392de011235a
MHargrove96 <MichaelH.96@yahoo.com>
2022-08-09 23:05:48 -0500

    redesign on the sign up page

--- src/components/NonUserHomeComponent/NonUserHome.jsx
+++ src/components/NonUserHomeComponent/NonUserHome.jsx
@@ -19,6 +19,11 @@ function NonUserHome({ setState }) {
   return (
     <div className={classes.pageContainer}>
       <section className={classes.infoContainer}>
+        <img
+          className={classes.heroImg}
+          src="https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1980&q=80"
+          alt="A wall of books on shelves."
+        />
         <h1 className={classes.infoTitle}>
           Welcome to your own personal Book Nook.
         </h1>
